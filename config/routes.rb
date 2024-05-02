f20653ba36adad3d3526b2d09b272e6be97144b3
Pinku Kumar Agrawal <Pinkuagarwal2831@gmail.com>
2024-05-02 21:50:48 +0530

    devise installed

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
+  root "home#index"
+  devise_for :users
   resources :trees
   get 'home/index'
-  root "home#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
